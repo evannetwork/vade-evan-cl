3e1df67570bea042d0ea0a4b19bb809358f403a1
wulfraem <wulfraem@users.noreply.github.com>
2021-04-08 15:59:01 +0200

    add typings

---
+++ typings/external.d.ts
@@ -0,0 +1,33 @@
+/*
+  Copyright (c) 2018-present evan GmbH.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+export type BigNumber = unknown;
+export type BlindedCredentialSecrets = unknown;
+export type BlindedCredentialSecretsCorrectnessProof = unknown;
+export type CredentialKeyCorrectnessProof = unknown;
+export type CredentialPrivateKey = unknown;
+export type CredentialPublicKey = unknown;
+export type CredentialSecretsBlindingFactors = unknown;
+export type CryptoCredentialSignature = unknown;
+export type MasterSecret = unknown;
+export type Nonce = unknown;
+export type RevocationKeyPrivate = unknown;
+export type RevocationKeyPublic = unknown;
+export type RevocationRegistry = unknown;
+export type RevocationRegistryDelta = unknown;
+export type RevocationTailsGenerator = unknown;
+export type SignatureCorrectnessProof = unknown;
+export type Witness = unknown;
