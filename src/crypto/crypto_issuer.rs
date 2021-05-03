d4aa101988b78652519e78accf54c79e34aec87e
Philip Kaiser <philip.kaiser@evan.team>
2021-05-03 10:21:39 +0200

    Prepare custom build for production (supports custom primes)

--- src/crypto/crypto_issuer.rs
+++ src/crypto/crypto_issuer.rs
@@ -46,8 +46,8 @@ impl Issuer {
 
     pub fn create_credential_definition(
         credential_schema: &CredentialSchema,
-        _p_safe: Option<&BigNumber>,
-        _q_safe: Option<&BigNumber>,
+        p_safe: Option<&BigNumber>,
+        q_safe: Option<&BigNumber>,
     ) -> Result<(CredentialPrivateKey, CryptoCredentialDefinition), Box<dyn Error>> {
         let mut non_credential_schema_builder =
             CryptoIssuer::new_non_credential_schema_builder()
@@ -78,19 +78,19 @@ impl Issuer {
             .map_err(|e| format!("could not finalize credential schema; {}", &e))?;
 
         let (public_key, credential_private_key, credential_key_correctness_proof) = {
-            // if p_safe.is_none() || q_safe.is_none() {
-            CryptoIssuer::new_credential_def(&crypto_schema, &non_credential_schema, true)
+            if p_safe.is_none() || q_safe.is_none() {
+                CryptoIssuer::new_credential_def(&crypto_schema, &non_credential_schema, true)
+                    .map_err(|e| format!("could not create credential definition; {}", &e))?
+            } else {
+                CryptoIssuer::new_credential_def_with_primes(
+                    &crypto_schema,
+                    &non_credential_schema,
+                    true,
+                    p_safe.ok_or("could not get prime number p_safe")?,
+                    q_safe.ok_or("could not get prime number q_safe")?,
+                )
                 .map_err(|e| format!("could not create credential definition; {}", &e))?
-            // } else {
-            //     CryptoIssuer::new_credential_def_with_primes(
-            //         &crypto_schema,
-            //         &non_credential_schema,
-            //         true,
-            //         p_safe.ok_or("could not get prime number p_safe")?,
-            //         q_safe.ok_or("could not get prime number q_safe")?,
-            //     )
-            //     .map_err(|e| format!("could not create credential definition; {}", &e))?
-            // }
+            }
         };
 
         let definition = CryptoCredentialDefinition {
